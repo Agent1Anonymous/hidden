27cb9d587a4cc5695ea9c1bfc173812162bb8d66
Agent1Anonymous <118023290+Agent1Anonymous@users.noreply.github.com>
2023-03-05 18:24:27 +0100

    Update $Project popCat.lua

--- $Project popCat.lua
+++ $Project popCat.lua
@@ -1,1 +1,3 @@
 print("Why would a good looking wealthy man move to the ʸᵒᵘ ᶠᵒᵘⁿᵈ ᵐᵉ country, become a veterinarian and marry a country hick?")
+
+-- DM me on Discord with a screenshot of the full text to get the role.
